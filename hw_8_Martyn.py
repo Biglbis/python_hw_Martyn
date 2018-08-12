57c49519695fb121b1cc7b4463a3bc18b8f780b7
Biglbis <Biglbis@gmail.com>
2018-08-12 07:28:47 +0300

    Innitial commit

---
+++ hw_8_Martyn.py
@@ -0,0 +1,11 @@
+"""
+Дана строка с именем студента, в которой имя предшествует фамилии, например  ‘Mark Zuckerberg‘.
+Написать программу, которая преобразует эту строку, ставя фамилию на первое место, а имя на второе.
+Т.е. ‘Mark Zuckerberg‘ -> ‘Zuckerberg Mark‘.
+"""
+name = 'Mark Zuckerberg'
+name_idx = name.find(' ')
+new_name = name[name_idx:] + ' ' + name[:name_idx]
+print(name, 'is the same as',new_name)
+
+
