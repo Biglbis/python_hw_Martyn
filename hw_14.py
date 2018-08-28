16bd1d52f34a3be43c9f34a41e562c97295ce44c
Biglbis <Biglbis@gmail.com>
2018-08-28 05:22:44 +0300

    Initial commitment

---
+++ hw_14.py
@@ -0,0 +1,10 @@
+"""
+Написать функцию, которая будет проверять четность некоторого числа.
+		def is_even(number): # returns boolean value
+"""
+
+def is_even (number):
+    return number % 2 == 0
+
+print (is_even (6))
+print (is_even (5))
